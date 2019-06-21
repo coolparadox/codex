4f0434675069d4ca3fa7c1d849f8b9568ae36998
Rafael Lorandi <coolparadox@gmail.com>
2019-06-21 02:00:48 -0300

    chunk blocks

--- bridge/Codexplain.hs
+++ bridge/Codexplain.hs
@@ -11,7 +11,7 @@ explain input = (explanation, extrusion) where
 
 data Document = MakeDocument Statistics [Block] deriving Show
 
-data Statistics = MakeStatistics (Map.Map String Int) (Map.Map String (Int, Int)) deriving Show
+data Statistics = MakeStatistics (Map.Map String Int) (Map.Map String Int) (Map.Map (String, Int) Int) deriving Show
 
 data ChunkLine =
     CodeLine String |
@@ -20,11 +20,12 @@ data ChunkLine =
 data Block =
     RawBlock [String] |
     CommentBlock [String] |
-    CodeFileBlock String Int [ChunkLine] deriving Show
+    CodeFileBlock String Int [ChunkLine] |
+    ChunkBlock String Int Int [ChunkLine] deriving Show
 
 parse :: String -> Document
 parse str = MakeDocument statistics promoted_blocks where
-    (promoted_blocks, statistics) = promoteCommentBlocks blocks (MakeStatistics Map.empty Map.empty)
+    (promoted_blocks, statistics) = promoteCommentBlocks blocks (MakeStatistics Map.empty Map.empty Map.empty)
     blocks = parseLowLevel $ lines str
 
 promoteCommentBlocks :: [Block] -> Statistics -> ([Block], Statistics)
@@ -38,23 +39,43 @@ promoteCommentBlock block@(CommentBlock (x:xs)) stat
     | take 4 x == "////" = (block, stat)
     | take 3 x == "///" = (block, stat)
     | take 2 x == "//" = promoteToCodeFileBlock (drop 2 x) xs stat
+    | take 1 x == "/" = promoteToChunkBlock (drop 1 x) xs stat
     | otherwise = (block, stat)
 promoteCommentBlock block stat = (block, stat)
 
+promoteToChunkBlock :: String -> [String] -> Statistics -> (Block, Statistics)
+promoteToChunkBlock name content stat@(MakeStatistics codefile_part_map chunk_form_map chunk_part_map) = (block, MakeStatistics codefile_part_map new_chunk_form_map new_chunk_part_map) where
+    block = ChunkBlock name form part chunk_lines
+    form = formFromMaybe m_form
+    (m_form, new_chunk_form_map) = Map.insertLookupWithKey keepChunkForm name 1 chunk_form_map
+    part = partFromMaybe m_part
+    (m_part, new_chunk_part_map) = Map.insertLookupWithKey incrementChunkPart (name, form) 1 chunk_part_map
+    chunk_lines = map (promoteToChunkLine stat) content 
+
+formFromMaybe :: Maybe Int -> Int
+formFromMaybe (Just form) = form
+formFromMaybe _ = 1
+
+keepChunkForm :: String -> Int -> Int -> Int
+keepChunkForm _ _ old = old
+
+incrementChunkPart :: (String, Int) -> Int -> Int -> Int
+incrementChunkPart _ _ part = part + 1
+
 promoteToCodeFileBlock :: String -> [String] -> Statistics -> (Block, Statistics)
-promoteToCodeFileBlock path content stat@(MakeStatistics codefile_map chunk_map) = (block, MakeStatistics new_codefile_map chunk_map) where
-    block = CodeFileBlock path count chunk_lines
-    (m_count, new_codefile_map) = Map.insertLookupWithKey incrementStatistic path 1 codefile_map
-    count = countFromMaybe m_count
+promoteToCodeFileBlock path content stat@(MakeStatistics codefile_part_map chunk_form_map chunk_part_map) = (block, MakeStatistics new_codefile_map chunk_form_map chunk_part_map) where
+    block = CodeFileBlock path part chunk_lines
+    (m_part, new_codefile_map) = Map.insertLookupWithKey incrementCodeFilePart path 1 codefile_part_map
+    part = partFromMaybe m_part
     chunk_lines = map (promoteToChunkLine stat) content
 
-promoteToChunkLine (MakeStatistics _ chunk_map) content_line
+promoteToChunkLine (MakeStatistics _ chunk_form_map _) content_line
     | text /= "" && head text == '/' = ChunkReference target form prefix
     | otherwise = CodeLine content_line
     where
-        (prefix, text) = unPrefix content_line
-        (form, _) = Map.findWithDefault (1, 1) target chunk_map
         target = tail text
+        (prefix, text) = unPrefix content_line
+        form = Map.findWithDefault 1 target chunk_form_map
 
 unPrefix :: String -> (String, String)
 unPrefix "" = ("", "")
@@ -67,12 +88,12 @@ unPrefix str@(c:cs)
 isBlank :: Char -> Bool
 isBlank c = c `elem` " \n"
 
-countFromMaybe :: Maybe Int -> Int
-countFromMaybe (Just n) = n + 1
-countFromMaybe _ = 1
+partFromMaybe :: Maybe Int -> Int
+partFromMaybe (Just part) = part + 1
+partFromMaybe _ = 1
 
-incrementStatistic :: String -> Int -> Int -> Int
-incrementStatistic _ _ value = value + 1
+incrementCodeFilePart :: String -> Int -> Int -> Int
+incrementCodeFilePart _ _ part = part + 1
 
 explainDocument :: Document -> String
 explainDocument (MakeDocument _ []) = ""
@@ -81,27 +102,61 @@ explainDocument (MakeDocument stat (block:blocks)) = (explainBlock block stat) +
 explainBlock :: Block -> Statistics -> String
 explainBlock (RawBlock strs) _ = unlines strs
 explainBlock (CommentBlock strs) _ = "////\n" ++ unlines strs ++ "////\n"
-explainBlock block@(CodeFileBlock path _ _) (MakeStatistics codefile_map _) = explainCodeFileBlock block (codefile_map Map.! path)
+explainBlock block@(CodeFileBlock path _ _) (MakeStatistics codefile_part_map _ _) = explainCodeFileBlock block part where
+    part = (codefile_part_map Map.! path)
+explainBlock block@(ChunkBlock name form _ _) (MakeStatistics _ chunk_form_map chunk_part_map) = explainChunkBlock block max_form max_part where
+    max_form = chunk_form_map Map.! name
+    max_part = chunk_part_map Map.! (name, form)
+
+explainChunkBlock :: Block -> Int -> Int -> String
+explainChunkBlock (ChunkBlock name form part content) max_form max_part = ""
+    ++ "." ++ render_name ++ sortingNote part max_part ++ "\n"
+    ++ "[#" ++ label_name ++ "]\n"
+    ++ "++++\n"
+    ++ "<div id=\"" ++ label_name ++ "\" class=\"exampleblock\" style=\"margin-bottom:1.25em;\">\n"
+    ++ "<div class=\"title\">" ++ render_name ++ sortingNote part max_part ++ "</div>\n"
+    ++ "<div class=\"content\" style=\"margin-bottom:.5em;\">\n"
+    ++ (unlines $ map explainChunkContentLine content)
+    ++ "</div>\n"
+    ++ "<div class=\"title\"><sup>"
+    ++ chunkNavigation (escapeChunkName name ++ "_" ++ show form) render_name part max_part
+    ++ "</sup></div>"
+    ++ "</div>\n"
+    ++ "++++\n"
+    where
+        render_name = name ++ " (form " ++ show form ++ ")"
+        label_name = chunkLabel name form part
+
+escapeChunkName :: String -> String
+escapeChunkName "" = ""
+escapeChunkName ('.':cs) = '_':(escapeChunkName cs)
+escapeChunkName (' ':cs) = '_':(escapeChunkName cs)
+escapeChunkName (c:cs) = c:(escapeChunkName cs)
+
+chunkLabel :: String -> Int -> Int -> String
+chunkLabel name form part = escapeChunkName name ++ "_" ++ show form ++ "_" ++ show part
 
 explainCodeFileBlock :: Block -> Int -> String
 explainCodeFileBlock (CodeFileBlock path part content) max_part = ""
     ++ "." ++ path ++ sortingNote part max_part ++ "\n"
-    ++ "[#" ++ targetLabel path part ++ "]\n"
+    ++ "[#" ++ label_name ++ "]\n"
     ++ "++++\n"
-    ++ "<div id=\"" ++ targetLabel path part ++ "\" class=\"exampleblock\" style=\"margin-bottom:1.25em;\">\n"
+    ++ "<div id=\"" ++ label_name ++ "\" class=\"exampleblock\" style=\"margin-bottom:1.25em;\">\n"
     ++ "<div class=\"title\">" ++ path ++ sortingNote part max_part ++ "</div>\n"
     ++ "<div class=\"content\" style=\"margin-bottom:.5em;\">\n"
     ++ (unlines $ map explainChunkContentLine content)
     ++ "</div>\n"
     ++ "<div class=\"title\"><sup>"
-    ++ codeFileNavigation path part max_part
+    ++ codeFileNavigation path path part max_part
     ++ "</sup></div>"
     ++ "</div>\n"
     ++ "++++\n"
+    where
+        label_name = codeFileLabel path part
 
-codeFileNavigation :: String -> Int -> Int -> String
-codeFileNavigation _ _ 1 = ""
-codeFileNavigation path part max_part = navigation ++ end_of_line where
+codeFileNavigation :: String -> String -> Int -> Int -> String
+codeFileNavigation _ _ _ 1 = ""
+codeFileNavigation label_name render_name part max_part = navigation ++ end_of_line where
     navigation = intercalate ", " navigators
     end_of_line = if navigation /= "" then ".<br>" else ""
     navigators = first ++ previous ++ next ++ last
@@ -109,20 +164,34 @@ codeFileNavigation path part max_part = navigation ++ end_of_line where
     previous = if part /= 1 then ["Previous: " ++ anchorize (part - 1)] else []
     next = if part /= max_part then ["Next: " ++ anchorize (part + 1)] else []
     last = if part < max_part - 1 then ["Last: " ++ anchorize max_part] else []
-    anchorize = codeFileHRef path max_part
+    anchorize = codeFileHRef label_name render_name max_part
 
-codeFileHRef :: String -> Int -> Int -> String
-codeFileHRef path max_part part = ""
+chunkNavigation :: String -> String -> Int -> Int -> String
+chunkNavigation _ _ _ 1 = ""
+chunkNavigation label_name render_name part max_part = navigation ++ end_of_line where
+    navigation = intercalate ", " navigators
+    end_of_line = if navigation /= "" then ".<br>" else ""
+    navigators = first ++ previous ++ next ++ last
+    first = if part > 2 then ["First: " ++ anchorize 1] else []
+    previous = if part /= 1 then ["Previous: " ++ anchorize (part - 1)] else []
+    next = if part /= max_part then ["Next: " ++ anchorize (part + 1)] else []
+    last = if part < max_part - 1 then ["Last: " ++ anchorize max_part] else []
+    anchorize = codeFileHRef label_name render_name max_part
+
+codeFileHRef :: String -> String -> Int -> Int -> String
+codeFileHRef label_name render_name max_part part = ""
     ++ "<a href=\"#"
-    ++ targetLabel path part
+    ++ codeFileLabel label_name part
     ++ "\">"
-    ++ path
+    ++ render_name
     ++ sortingNote part max_part
     ++ "</a>"
 
 explainChunkContentLine :: ChunkLine -> String
-explainChunkContentLine (ChunkReference target form prefix) =
-    "<code class=\"codex\">" ++ htmlEscape prefix ++ "</code><em><a href=\"#" ++ targetLabel target form ++ "\">" ++ target ++ "</a></em><span class=\"codex\">&crarr;</span><br>"
+explainChunkContentLine (ChunkReference target form prefix) = ""
+    ++ "<code class=\"codex\">" ++ htmlEscape prefix ++ "</code>"
+    ++ "<em><a href=\"#" ++ codeFileLabel target form ++ "_1\">" ++ target ++ "</a></em>"
+    ++ "<span class=\"codex\">&crarr;</span><br>"
 explainChunkContentLine (CodeLine str) =
     "<code class=\"codex\">" ++ htmlEscape str ++ "</code><span class=\"codex\">&crarr;</span><br>"
 
@@ -136,10 +205,11 @@ htmlEscape ('&':cs) = "&amp;" ++ htmlEscape cs
 htmlEscape (' ':cs) = "&nbsp;" ++ htmlEscape cs
 htmlEscape (c:cs) = c:(htmlEscape cs)
 
-targetLabel :: String -> Int -> String
-targetLabel "" index = "_" ++ show index
-targetLabel ('.':cs) index = '_':(targetLabel cs index)
-targetLabel (c:cs) index = c:(targetLabel cs index)
+codeFileLabel :: String -> Int -> String
+codeFileLabel "" part = "_" ++ show part
+codeFileLabel (' ':cs) part = '_':(codeFileLabel cs part)
+codeFileLabel ('.':cs) part = '_':(codeFileLabel cs part)
+codeFileLabel (c:cs) part = c:(codeFileLabel cs part)
 
 sortingNote :: Int -> Int -> String
 sortingNote _ 1 = ""

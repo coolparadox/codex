ab484e94349fc1926cbd8c145b3d0fe1e7d7167d
Rafael Lorandi <coolparadox@gmail.com>
2019-12-08 21:51:54 -0300

    Move master content to cpp branch

--- bridge/codexpand.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import System.IO
-import Codexpand
-
-main :: IO()
-main = do
-    c <- getContents
-    sequence_ (actionize (parse c))
--- inception/fdopen/fdopen.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-
-int main() {
-    FILE* out;
-    out = fdopen(3, "w");
-    if (!out) return 1;
-    fprintf(out, "yo!\n");
-    return 0;
-}
